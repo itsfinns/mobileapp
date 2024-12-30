3733f46a278de5e6c3b1e9e3119e82a419d81bb0
finn <finnemans12@gmail.com>
2024-12-30 15:25:16 +0100

    test voor woorden generator

---
+++ Database.cs
@@ -0,0 +1,33 @@
+﻿using SQLite;
+using System.IO;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+public class DatabaseService
+{
+    private SQLiteAsyncConnection _database;
+
+    public DatabaseService()
+    {
+        var dbPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "words.db");
+        _database = new SQLiteAsyncConnection(dbPath);
+        _database.CreateTableAsync<Word>().Wait();
+    }
+
+    public Task<int> AddWordAsync(Word word)
+    {
+        return _database.InsertAsync(word);
+    }
+
+    public Task<List<Word>> GetWordsAsync()
+    {
+        return _database.Table<Word>().ToListAsync();
+    }
+}
+
+public class Word
+{
+    [PrimaryKey, AutoIncrement]
+    public int Id { get; set; }
+    public string Text { get; set; }
+}
--- GamePage.xaml.cs
+++ GamePage.xaml.cs
@@ -6,4 +6,36 @@ public partial class GamePage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+	private async void OnGetWordClicked(object sender, EventArgs e)
+	{
+        base.OnAppearing();
+
+        var dbService = new DatabaseService();
+        var words = await dbService.GetWordsAsync();
+
+        if (words.Count == 0) // Voeg woorden toe als de database leeg is
+        {
+            await dbService.AddWordAsync(new Word { Text = "Appel" });
+            await dbService.AddWordAsync(new Word { Text = "Banaan" });
+            await dbService.AddWordAsync(new Word { Text = "Kers" });
+        }
+    }
+    private async void OnRandomWordButtonClicked(object sender, EventArgs e)
+    {
+        var dbService = new DatabaseService();
+        var words = await dbService.GetWordsAsync();
+
+        if (words.Count > 0)
+        {
+            var random = new Random();
+            var randomWord = words[random.Next(words.Count)];
+            GetWordButton.Text = randomWord.Text;
+        }
+        else
+        {
+            GetWordButton.Text = "Geen woorden beschikbaar!";
+        }
+    }
+
 }
